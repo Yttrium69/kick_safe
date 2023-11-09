b6ff2397436acc3f5f554fdb96442a3d4ab821a7
Yttrium69 <47668106+Yttrium69@users.noreply.github.com>
2023-11-09 12:54:51 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Home from './view/Home';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/view/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.scss'
+
+
+function Home(): JSX.Element{
+    return (
+        <div className = "page_home">
+          <h1>Welcome to the Home Page</h1>
+          <p>This is a simple React component written in TypeScript.</p>
+        </div>
+      );
+}
+
+export default Home;

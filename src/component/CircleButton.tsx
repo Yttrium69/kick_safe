86a59a811f50b1e36c1fba754ee4ff0aa51f6bf8
kimminsum <tommy1005a@gmail.com>
2023-11-18 16:53:31 +0900

    circle button

--- src/component/CircleButton.tsx
+++ src/component/CircleButton.tsx
@@ -14,14 +14,18 @@ function CircleButton({ handleClick = () => {}, isActivated = false }: CircleBut
         handleClick(); // Call the parent component's click handler
     };
 
-    return (
-        // <div className={`toggle-button ${isActivated ? 'activated' : ''}`} onClick={handleClickInternal}>
-        //     <img src={isActivated ? iconChecked : iconNotChecked} alt="Toggle Icon" />
-        // </div>
-        <div style={{cursor:'pointer', position: 'relative', display: 'flex', justifyContent: 'center', alignItems:'center',width: '1.75rem', height: '1.75rem', borderRadius:'10rem', border:'1px solid #949BA4'}} className='btn_outer'>
-            <div style={{width: '1.25rem', height: '1.25rem', backgroundColor:'#FFE55E', borderRadius:'10rem'}} className='btn_inner'></div>
-        </div>
-    );
+    if (isActivated === true) {
+        return (
+            <div style={{cursor:'pointer', position: 'relative', display: 'flex', justifyContent: 'center', alignItems:'center',width: '1.75rem', height: '1.75rem', borderRadius:'10rem', border:'1px solid #949BA4'}} className='btn_outer' onClick={handleClickInternal}>
+                <div style={{width: '1.25rem', height: '1.25rem', backgroundColor:'#FFE55E', borderRadius:'10rem'}} className='btn_inner'></div>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div style={{cursor:'pointer', position: 'relative', display: 'flex', justifyContent: 'center', alignItems:'center',width: '1.75rem', height: '1.75rem', borderRadius:'10rem', border:'1px solid #949BA4'}} className='btn_outer' onClick={handleClickInternal}></div>
+        );
+    }
 }
 
 export default CircleButton;

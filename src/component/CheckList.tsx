e44f84755b36b11d711aff3e924cb5a9798ab4ed
kimminsum <tommy1005a@gmail.com>
2023-11-18 16:44:04 +0900

    logo

--- src/component/CheckList.tsx
+++ src/component/CheckList.tsx
@@ -1,17 +1,18 @@
 // CheckList.tsx
 import React, { FC } from 'react';
-import imgHelmet from '../images/img_helmet.png';
+// import imgHelmet from '../images/img_helmet.png';
 
 interface CheckListProps {
     title: string;
     description: string;
+    img_path: string;
 }
 
-const CheckList: FC<CheckListProps> = ({ title, description }) => {
+const CheckList: FC<CheckListProps> = ({ title, description, img_path }) => {
     return (
         <div className="check_list_container">
             <div className="top">
-                <img className="helmet_img" src={imgHelmet} alt="Helmet" />
+                <img className="helmet_img" src={img_path} alt="Helmet" />
                 <div className="title">{title}</div>
             </div>
             <div className="bottom">{description}</div>

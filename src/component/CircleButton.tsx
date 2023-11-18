6d90ce5ed3c566f47dbeac4cb40b679d5037f1e4
Yttrium69 <47668106+Yttrium69@users.noreply.github.com>
2023-11-18 16:37:26 +0900

    check btn styled

--- src/component/CircleButton.tsx
+++ src/component/CircleButton.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import iconChecked from '../images/icon_checked.svg';
 import iconNotChecked from '../images/icon_not_checked.svg';
+import { relative } from 'path';
 
 interface CircleButtonProps {
     handleClick?: () => void;
@@ -14,8 +15,11 @@ function CircleButton({ handleClick = () => {}, isActivated = false }: CircleBut
     };
 
     return (
-        <div className={`toggle-button ${isActivated ? 'activated' : ''}`} onClick={handleClickInternal}>
-            <img src={isActivated ? iconChecked : iconNotChecked} alt="Toggle Icon" />
+        // <div className={`toggle-button ${isActivated ? 'activated' : ''}`} onClick={handleClickInternal}>
+        //     <img src={isActivated ? iconChecked : iconNotChecked} alt="Toggle Icon" />
+        // </div>
+        <div style={{cursor:'pointer', position: 'relative', display: 'flex', justifyContent: 'center', alignItems:'center',width: '1.75rem', height: '1.75rem', borderRadius:'10rem', border:'1px solid #949BA4'}} className='btn_outer'>
+            <div style={{width: '1.25rem', height: '1.25rem', backgroundColor:'#FFE55E', borderRadius:'10rem'}} className='btn_inner'></div>
         </div>
     );
 }
